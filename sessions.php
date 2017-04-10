f08e9ef3823b5c230221272e65472af163db759d
gheorghestrimtu <studentcuza@gmail.com>
2017-04-11 00:56:49 +0300

    added pagination to session viewer
    + filtering by sorting

--- sessions.php
+++ sessions.php
@@ -3,6 +3,24 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html>
+<head>
+	<title>jQuery plugin: Tablesorter 2.0 - Pager plugin</title>
+	<link rel="stylesheet" href="http://tablesorter.com/docs/css/jq.css" type="text/css" media="print, projection, screen" />
+	<link rel="stylesheet" href="http://tablesorter.com/themes/blue/style.css" type="text/css" media="print, projection, screen" />
+	<script type="text/javascript" src="http://tablesorter.com/jquery-latest.js"></script>
+	<script type="text/javascript" src="http://tablesorter.com/__jquery.tablesorter.js"></script>
+	<script type="text/javascript" src="http://tablesorter.com/addons/pager/jquery.tablesorter.pager.js"></script>
+	<script type="text/javascript" src="http://tablesorter.com/docs/js/chili/chili-1.8b.js"></script>
+	<script type="text/javascript" src="http://tablesorter.com/docs/js/docs.js"></script>
+	<script type="text/javascript">
+	$(function() {
+		$("#myTable")
+			.tablesorter({widthFixed: true, widgets: ['zebra']})
+			.tablesorterPager({container: $("#pager")});
+	});
+	</script>
+	<link rel="stylesheet" type="text/css" href="http://tablesorter.com/docs/js/chili/javascript.css">
+</head>
 <body>
 
 <?php
@@ -42,7 +60,9 @@ if (!$r2) {
 }
 
 // Fetch the results of the query
-print "<table border='1'>\n";
+print('<table id="myTable" class="tablesorter" border="1">');
+print('<thead><tr><th>1</th><th>2</th><th>3</th><th>4</th></thead>');
+print('<tbody>');
 while ($row = oci_fetch_array($stid2, OCI_ASSOC+OCI_RETURN_NULLS)) {
     print "<tr>\n";
     foreach ($row as $item) {
@@ -50,12 +70,35 @@ while ($row = oci_fetch_array($stid2, OCI_ASSOC+OCI_RETURN_NULLS)) {
     }
     print "</tr>\n";
 }
-print "</table>\n";
+print('</tbody>');
+print "</table>";
 
 oci_free_statement($stid2);
 
 oci_close($conn);
 ?>
 
+<div id="pager" class="pager">
+	<form>
+		<img src="http://tablesorter.com/addons/pager/icons/first.png" class="first"/>
+		<img src="http://tablesorter.com/addons/pager/icons/prev.png" class="prev"/>
+		<input type="text" class="pagedisplay"/>
+		<img src="http://tablesorter.com/addons/pager/icons/next.png" class="next"/>
+		<img src="http://tablesorter.com/addons/pager/icons/last.png" class="last"/>
+		<select class="pagesize">
+			<option selected="selected"  value="10">10</option>
+			<option value="20">20</option>
+			<option value="30">30</option>
+			<option  value="40">40</option>
+		</select>
+	</form>
+</div>
+
+</div>
+<script src="http://www.google-analytics.com/urchin.js" type="text/javascript"></script>
+<script type="text/javascript">
+_uacct = "UA-2189649-2";
+urchinTracker();
+</script>
 </body>
 </html>
